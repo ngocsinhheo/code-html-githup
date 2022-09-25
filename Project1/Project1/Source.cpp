11cf302db1040612338586f5e642c76ae6130856
ngocsinhheo <ngocsinhheo@gmail.com>
2022-09-25 09:51:48 +0700

    fisrt commit

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int x;
+	cout << "hello"<<endl;
+	cin >> x;
+	cout <<"so vua nhap la "<<x;
+	return 0;
+
+}
---
+++ helloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    
+    scanf("$d",x);
+    printf("so vua nhap la $d",x);
+    return 0;
+    
+}
